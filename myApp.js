8782187b0c858656358422d9ea4c6977f81345a0
Barnabas James <barnabasjames27@gmail.com>
2025-07-24 21:06:58 +0100

    Correct?

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
-let express = require("express");
-let app = express();
 require("dotenv").config();
 
+let express = require("express");
+let app = express();
+
 app.get("/", (req, res) => {
-  // res.send("Hello Express");
   res.sendFile(__dirname + "/views/index.html");
 
   app.use("/public", express.static(__dirname + "/public"));
