316eff843aa4260a6bd53b6f2fcb0e16232dab1f
Barnabas James <barnabasjames27@gmail.com>
2025-07-24 18:55:16 +0100

    no 4

--- myApp.js
+++ myApp.js
@@ -4,6 +4,8 @@ let app = express();
 app.get("/", (req, res) => {
   // res.send("Hello Express");
   res.sendFile(__dirname + "/views/index.html");
+
+  app.use("/public", express.static(__dirname + "/public"));
 });
 console.log("Hello World!");
 
