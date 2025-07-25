1f191fa5909f5d6848c0338d9be2e30e43ec1402
Barnabas James <barnabasjames27@gmail.com>
2025-07-25 16:15:40 +0100

    no 11 i think?

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
+let bodyParser = require("body-parser");
 let express = require("express");
 let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
