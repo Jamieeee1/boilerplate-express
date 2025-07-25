fcb8f826310fe114f231c779826f97662975c6b0
Barnabas James <barnabasjames27@gmail.com>
2025-07-25 15:26:03 +0100

    no 8?

--- myApp.js
+++ myApp.js
@@ -20,6 +20,17 @@ app.get("/json", (req, res) => {
   });
 });
 
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({ time: req.time });
+  }
+);
+
 console.log("Hello World!");
 
 module.exports = app;
