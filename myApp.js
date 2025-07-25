2047e213677e00a38a04c7e5e3b224a41e50e97b
Barnabas James <barnabasjames27@gmail.com>
2025-07-25 17:22:55 +0100

    lesson 12 lets goooo!!!!

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let bodyParser = require("body-parser");
 let express = require("express");
 let app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
@@ -39,11 +39,18 @@ app.get("/:word/echo", (req, res, next) => {
   res.send({ echo: word });
 });
 
-app.route("/name").get((req, res) => {
-  const firstName = req.query.first;
-  const lastName = req.query.last;
-  res.json({ name: `${firstName} ${lastName}` });
-});
+app
+  .route("/name")
+  .get((req, res) => {
+    const firstName = req.query.first;
+    const lastName = req.query.last;
+    res.json({ name: `${firstName} ${lastName}` });
+  })
+  .post((req, res) => {
+    const firstName = req.body.first;
+    const lastName = req.body.last;
+    res.json({ name: `${firstName} ${lastName}` });
+  });
 
 console.log("Hello World!");
 
